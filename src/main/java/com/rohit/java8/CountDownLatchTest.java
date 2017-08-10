4bc3a121687341e669fa1b9de0c68d1f01e461d5
Rohit <rohitrohatgi@gmail.com>
2017-08-10 15:50:55 +0800

    Initial commit

---
+++ src/main/java/com/rohit/java8/CountDownLatchTest.java
@@ -0,0 +1,36 @@
+package com.rohit.java8;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by Rohit on 10-08-2017.
+ * This code is only used for learning purpose.
+ */
+public class CountDownLatchTest {
+    public static void main(String[] args) throws Exception {
+        ExecutorService service = Executors.newFixedThreadPool(5);
+        final CountDownLatch latch1 = new CountDownLatch(5);
+        final CountDownLatch latch2 = new CountDownLatch(5);
+
+        for(int x = 0; x < 5; x++)
+            service.submit(() -> {
+                try {
+                    System.out.println(Thread.currentThread().getName());
+                    latch2.countDown();
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                latch1.countDown();
+            });
+
+
+        latch2.await();
+        System.out.println("Latch2 Done");
+
+        latch1.await();
+        System.out.println("Latch1 Done");
+    }
+}
